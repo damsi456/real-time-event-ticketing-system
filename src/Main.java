612f28974a1da089336881e4a1041f93184046ae
Damsiha Kumarasinghe <155002269+damsi69@users.noreply.github.com>
2024-11-28 07:44:17 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        CLIController cliController = new CLIController();
+        cliController.startCLI();
+    }
+}
