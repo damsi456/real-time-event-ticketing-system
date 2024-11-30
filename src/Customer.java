0a7d58dbbb62d4766aeb17a223feeb0b8cf0615a
Damsiha Kumarasinghe <155002269+damsi69@users.noreply.github.com>
2024-11-30 14:35:39 +0530

    Created Customer

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,31 @@
-public class Customer {
+import java.util.Random;
+
+public class Customer implements Runnable{
+    private final TicketPool ticketPool;
+    private final int customerRetrievalRate;
+    private Random random;
+
+    public Customer(TicketPool ticketPool, int customerRetrievalRate) {
+        this.ticketPool = ticketPool;
+        this.customerRetrievalRate = customerRetrievalRate;
+        this.random = new Random();
+    }
+
+    @Override
+    public void run() {
+        try{
+            while (!Thread.currentThread().isInterrupted()){
+                int ticketsToRemove = random.nextInt(3) + 1;
+                boolean success = ticketPool.removeTickets(ticketsToRemove);
+
+                if(!success){
+                    System.out.println("There's not enough tickets to buy.");
+                }
+
+                Thread.sleep(customerRetrievalRate);
+            }
+        } catch (InterruptedException e){
+            System.out.println("");
+        }
+    }
 }
